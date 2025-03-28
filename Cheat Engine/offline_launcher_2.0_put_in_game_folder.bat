172f991f58ac0b1af29472bfa45e8b4e95006429
ProGamerXDP <igottwentycharacters@gmail.com>
2025-03-28 17:23:11 +0100

    update .bat file

--- Cheat Engine/offline_launcher_2.0_put_in_game_folder.bat
+++ Cheat Engine/offline_launcher_2.0_put_in_game_folder.bat
@@ -1,6 +1,16 @@
 @echo off
 taskkill /IM EasyAntiCheat_EOS.exe /F
+
 set SteamAppId=1245620
 set SteamGameId=1245620
+
+cd /d "C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game"
 if not exist eldenring.exe cd Game
 start eldenring.exe
+
+start "" "%~dp0CHAOS MOD.CT"
+
+cd /d "%~dp0..\\Rest"
+node main.js
+
+pause
